9cac4c5d08a0728149d8571d4ff7ee3e20142e79
Ökkeş Aksoy <49321543+Aksoyhlc@users.noreply.github.com>
2019-04-06 01:50:52 +0300

    Aksoyhlc İş-Proje Takip Scripti
    Aksoyhlc İş-Proje Takip Scripti

---
+++ islemler/baglan.php
@@ -0,0 +1,18 @@
+<?php 
+$host="localhost"; //Host adınızı girin varsayılan olarak Localhosttur eğer bilginiz yoksa bu şekilde bırakın
+$veritabani_ismi="projetakip"; //Veritabanı İsminiz
+$kullanici_adi="root"; //Veritabanı kullanıcı adınız
+$sifre="3762Aksoyhlc."; //Kullanıcı şifreniz
+
+try {
+
+	$db=new PDO("mysql:host=$host;dbname=$veritabani_ismi;charset=utf8",$kullanici_adi,$sifre);
+	//echo "veritabanı bağlantısı başarılı";
+}
+
+catch (PDOExpception $e) {
+
+	echo $e->getMessage();
+}
+
+?>
